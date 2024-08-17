7df875fdcd977743128c1c5b39caac6a5622396d
Wilson Waters <wilson.waters@makerx.com.au>
2024-08-17 22:57:35 +0800

    cleanup

--- shelly-energy-logger.py
+++ shelly-energy-logger.py
@@ -1,33 +1,4 @@
-"""Log energy of Shelly gen 2 devices consumption to CSV file.
-
-A CSV file is created to hold readings and includes a breakdown of energy consumption per period (default per hour). All time operates as localtime of the device this runs on.
-
-Example:
-
-```
-'Timestamp', 'Cumulative Energy Consumed (Wh)', 'Energy Consumed Last Period (Wh)', 'Cost'
-2024-08-17 21:00:00, 22374.124, 0, 0
-2024-08-17 22:00:00, 22375.412, 1.288, 0
-2024-08-17 23:00:00, 22376.596, 1.184, 0
-```
-
-This file is rotated monthly and is renamed appropriately.
-
-Some use cases include
-- Logging energy consumption of a electric vehicle charger for tax purposes
-- more?
-
-Tested with Shelly Pro 1PM. This uses the GetStatus API https://shelly-api-docs.shelly.cloud/gen2/ComponentsAndServices/Shelly#shellygetstatus for wider device support.
-It is also possible to use the EnergyMonitor component (https://shelly-api-docs.shelly.cloud/gen2/ComponentsAndServices/EM) for devices which this method is supported
-
-# Installing
-- Ensure the device has latest firmware (tested with 1.4.0)
-- Install the requests library `pip install requests` (potentially in a virtual environment - `.\.venv\Scripts\activate `)
-
-
-Requirements
-- requests - for http API access https://pypi.org/project/requests/
-- apscheduler - for cron-like scheduling https://pypi.org/project/APScheduler/
+"""Log energy consumption of Shelly gen 2 devices consumption to CSV file.
 
 Copyright (c) Wilson Waters 2024.
 """
